0b1c35952ba4ded411b4bccd5db7b9b093b6d8cc
Melih Cüneyter <melihcuneyter@hotmail.com>
2023-01-10 14:09:35 +0300

    Initial project.

---
+++ week6hw3/week6hw3/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  week6hw3
+//
+//  Created by Melih Cüneyter on 10.01.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ week6hw3/week6hw3/BaseVC.swift
@@ -0,0 +1,16 @@
+//
+//  BaseVC.swift
+//  week6hw3
+//
+//  Created by Melih Cüneyter on 10.01.2023.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
